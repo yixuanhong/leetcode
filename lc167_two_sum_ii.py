4b47f6a301f471038b93d3d99412eb96048a8cbc
yixuanhong <hongyixuan629@gmail.com>
2025-10-08 00:11:19 -0400

    Week1 (#1)
    * lc167
    
    * lc238
    
    * lc75

---
+++ lc167_two_sum_ii.py
@@ -0,0 +1,13 @@
+def two_sum(numbers: list[int], target: int) -> list[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        curr_sum = numbers[left] + numbers[right]
+        
+        if curr_sum == target:
+            return [left + 1, right + 1]  # 1-indexed
+        elif curr_sum < target:
+            left += 1
+        else:
+            right -= 1
+
